6ec7a1e2ecdd704d20153f681c0a18c4eef1ef1a
SanB <sandro.brignoli@outlook.fr>
2022-11-24 10:53:05 +0100

    🚀 Link to Backend Render Deployment

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@ module.exports = {
   reactStrictMode: true,
   env: {
     // URL: "http://localhost:5000/api",
-    URL: "https://sanb-quizz-server.herokuapp.com/api",
+    // URL: "https://sanb-quizz-server.herokuapp.com/api",
+    URL: "https://sanb-quizz.onrender.com/api",
   },
 };
